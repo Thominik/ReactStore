6bdceaf00a573651425554ea89dfecace2c456a9
Thominik <dominikbednarz.ti@gmail.com>
2022-10-25 16:41:27 +0200

    Header, cards etc

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,6 +25,7 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -39,6 +40,11 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(opt =>
+            {
+                opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
 
             app.UseAuthorization();
 
